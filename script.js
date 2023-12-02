c3743a04221dac64bec11181b30d2ab2bcc92111
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2023-12-01 19:30:52 -0600

    date and time jquery

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+$(document).ready(function() {
+  function updateDateTime() {
+    var currentDay = $('#currentDay');
+    var now = dayjs().format('dddd, MMMM D, YYYY hh:mm:ss A');
+
+    currentDay.text(now);
+  }
+
+  updateDateTime();
+
+  setInterval(updateDateTime, 1000);
+});
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +37,46 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+//PSUEDOCODE
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// How can I get the current day and time to be displayed/updating?
+// What method gets the time
+// How do I display it
+//DONE____________________________________________________________
+
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours of 9am to 5pm
+//I believe this is already done with the HTML I added
+//DONE_____________________________________________________________
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+// How can I get the time and block to interact
+// if (time block (Hour) < current time (HH)) {
+//  background color changes to past
+// } else if (time block (hour) === curent time (HH)) {
+//  backgroud color changes to present
+// } else (time block(hour) > current time (HH)) {
+//  background color changes to future
+// }
+
+// WHEN I click into a time block
+// THEN I can enter an event
+
+// Already able to do that because of the textarea element
+
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+
+// I would need to add an event listener for the buttons
+// The function in that event listener will save it to local storage
+// Do I need to write another function to have the button display what is in localStorage?
+// If that's the case, do I need to have each button be a different class?
+
+// WHEN I refresh the page
+// THEN the saved events persist
+// Maybe this is the part to display
